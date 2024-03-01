84db2e26e63f43380eea7caceb6199943afa6e6c
Bas [Panwan] <panwan.21st@gmail.com>
2024-03-01 16:31:48 +0700

    db

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'sqlite',
-    storage: './Database/SQBooks.sqlite',
+    storage: './Database/SQGallerysqlite',
     logging: console.log,
 });
 
